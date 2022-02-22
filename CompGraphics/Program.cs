e0d7a657c2ba27057e60d9535fe638ce47aeab0c
gazinaft <gazinaft@gmail.com>
2022-02-22 18:37:44 +0200

    Init

---
+++ CompGraphics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompGraphics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
