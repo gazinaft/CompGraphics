6edce85988bbd055c112900513d879181d11e1ed
Mihailo <mihailpro02@gmail.com>
2022-05-05 22:04:06 +0300

    added light and normal

---
+++ GeometricObjects/Basic/DirectionalLight.cs
@@ -0,0 +1,13 @@
+﻿namespace GeometricObjects.Basic
+{
+    class DirectionalLight
+    {
+        private Vector direction;
+
+        public Vector Direction
+        {
+            get => direction;
+            set => direction = value.Normalize();
+        }
+    }
+}
