30a3a08e252876e25fe2884d7aa667b39ad198f5
Mihailo <mihailpro02@gmail.com>
2022-05-08 04:22:11 +0300

    added plane and vector fucctionality unit tests

--- GeometricObjects/Basic/Vertex.cs
+++ GeometricObjects/Basic/Vertex.cs
@@ -6,6 +6,18 @@
         public double Y { get; set; }
         public double Z { get; set; }
 
+        public Vertex()
+        {
+
+        }
+
+        public Vertex(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
         public static Vector operator -(Vertex p1, Vertex p2)
         {
             return new Vector(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
