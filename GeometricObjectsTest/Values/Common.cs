f5d73ecad5f75f5c1462ce484469dd34f5334e9c
gazinaft <gazinaft@gmail.com>
2022-05-12 23:42:15 +0300

    Set up tests

--- GeometricObjectsTest/Values/Common.cs
+++ GeometricObjectsTest/Values/Common.cs
@@ -19,6 +19,7 @@ namespace GeometricObjectsTest.Values
         public Ray Ray3 => new() { Direction = Vector2.Normalize(), Origin = Vertex4 };
         public Ray Ray4 => new() { Direction = Vector3.Normalize(), Origin = Vertex1 };
         public Ray Ray5 => new() { Direction = Vector4.Normalize(), Origin = Vertex5 };
+        public Ray Ray6 =>  new() { Direction = Vector4.Normalize(), Origin = Vertex6 };
 
         public Sphere Sphere1 => new() { };
 
@@ -26,11 +27,22 @@ namespace GeometricObjectsTest.Values
         public Vector Vector2 => new(-3, -4, -12);
         public Vector Vector3 => new(1, 1, 1);
         public Vector Vector4 => new(1, -1, 0);
+        public Vector Vector5 => new(-1, 1, 0);
 
         public Vertex Vertex1 => new(0, 0, 0);
         public Vertex Vertex2 => new(0, 0, -10);
         public Vertex Vertex3 => new(0, 0, 10);
         public Vertex Vertex4 => new(3, 4, 12);
         public Vertex Vertex5 => new(3, 3, 3);
+        public Vertex Vertex6 => new(-1, 1, 0);
+        public Vertex Vertex7 => new(4, 1, 0);
+
+        
+
+        public Triangle Triangle1 => new Triangle(Vertex1, Vertex2, Vertex5);
+        public Triangle Triangle2 => new Triangle(Vertex1, Vertex5, Vertex2);
+        public Triangle Triangle3 => new Triangle(Vertex3, Vertex4, Vertex5);
+        public Triangle Triangle4 => new Triangle(Vertex3, Vertex5, Vertex4);
+        
     }
 }
