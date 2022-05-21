f29a2462c948edf7fe6c4c6b69618241f597110f
Mihailo <mihailpro02@gmail.com>
2022-05-22 00:11:00 +0300

    Sphere tests

---
+++ GeometricObjectsTest/Sources/Sphere_Intersects_Inside_Source.cs
@@ -0,0 +1,17 @@
+﻿using GeometricObjects.Basic;
+using GeometricObjects.Figures;
+
+namespace GeometricObjectsTest.Sources
+{
+    class Sphere_Intersects_Inside_Source : Source<Sphere_Intersects_Inside_Source.Args>
+    {
+        protected override string FileName => @"Sphere-InsideIntersection-Values.json";
+
+        internal class Args
+        {
+            public Sphere? Sphere { get; set; }
+            public Ray? Ray { get; set; }
+            public double Expected { get; set; }
+        }
+    }
+}
