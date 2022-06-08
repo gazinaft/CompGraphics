9e192eefffe002c50a3bc7572c9a0733b44b2ddf
gazinaft <gazinaft@gmail.com>
2022-06-08 17:43:36 +0300

    Basic ppm writer

---
+++ CompGraphics/PpmWriter.cs
@@ -0,0 +1,33 @@
+using System.Drawing;
+using System.IO;
+
+namespace CompGraphics;
+
+public class PpmWriter: IWriter
+{
+    private string _path;
+    
+    public PpmWriter(string path)
+    {
+        _path = path;
+    }
+    
+    public void Write(Color[,] pixels)
+    {
+        StreamWriter file = new(_path);
+        file.WriteLine("P3");
+        file.WriteLine(pixels.GetLength(0) + " " + pixels.GetLength(1));
+        file.WriteLine("255");
+        file.WriteLine();
+        for (int i = 0; i < pixels.GetLength(1); i++)
+        {
+            for (int j = 0; j < pixels.GetLength(0); j++)
+            {
+                var p = pixels[j, i];
+                file.WriteLine(p.R + " " + p.G + " " + p.B);
+            }
+        }
+        file.Flush();
+        file.Close();
+    }
+}
--- CompGraphics/Program.cs
+++ CompGraphics/Program.cs
@@ -13,17 +13,17 @@ namespace CompGraphics
         {
             var reader = new HardCodeReader();
             reader
-                .Add(new Sphere() { Center = new Vertex(-5, 0, 0), Radius = 10 })
-                .Add(new Plane() { Normal = new Vector(1, 3, 1).Normalize(), Point = new Vertex(-2, 0, 0) });
+                .Add(new Sphere() { Center = new Vertex(-5, 0, 0), Radius = 10 });
+                // .Add(new Plane() { Normal = new Vector(1, 3, 1).Normalize(), Point = new Vertex(-2, 0, 0) });
                 // .Add(new Triangle(new Vertex(1, -10, 10), new Vertex(1, -10, -10), new Vertex(1, 10, 0)));
                 
             var tracer = new SimpleTracer(
-                new Camera(120, 40) {Pov = new Vertex(20, 0, 0)},
+                new Camera(300, 300) {Pov = new Vertex(20, 0, 0)},
                 new DirectionalLight() {Direction = new Vector(-1, -1, 1)},
                 new SimpleCrossFinder(),
-                Color.Black
+                Color.LightBlue
                 );
-            var writer = new ConsoleWriter();
+            var writer = new PpmWriter("/run/media/gazinaft/d/prog/graphics/CompGraphics/Figures.ppm");
 
             writer.Write(tracer.Trace(reader.Read(PATH)));
         }
