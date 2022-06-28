f9e60f9df5d07a5590144f4c0d4a7befd37e58a6
gazinaft <gazinaft@gmail.com>
2022-06-28 10:24:07 +0300

    Fix namespaces

--- CompGraphics/AmbientLight.cs
+++ CompGraphics/AmbientLight.cs
@@ -3,29 +3,34 @@ using System.Drawing;
 using Core;
 using GeometricObjects.Basic;
 
-namespace CompGraphics;
-
-public class AmbientLight: ILighting
+namespace CompGraphics
 {
-    public Color LColor { get; set; }
-    public float Intensity { get; set; }
-    private Random _rand = new Random();
 
-    
-    public Ray ShadowRay(Vector norm, Vertex crossPoint)
+    public class AmbientLight : ILighting
     {
-        var randVec = new Vector(_rand.NextDouble() * 10 - 5, _rand.NextDouble() * 10 - 5, _rand.NextDouble() * 10 - 5);
-        if (randVec.Dot(norm) < 0)
+        public Color LColor { get; set; }
+        public float Intensity { get; set; }
+        private Random _rand = new Random();
+
+
+        public Ray ShadowRay(Vector norm, Vertex crossPoint)
         {
-            randVec = randVec.Scale(-1);
-        }
-        return new Ray() { Origin = crossPoint, Direction = randVec };
-    }
+            var randVec = new Vector(_rand.NextDouble() * 10 - 5, _rand.NextDouble() * 10 - 5,
+                _rand.NextDouble() * 10 - 5);
+            if (randVec.Dot(norm) < 0)
+            {
+                randVec = randVec.Scale(-1);
+            }
 
-    
-    
-    public Color OutLight(Vector norm, Vertex crossPoint)
-    {
-        return Color.FromArgb((int)(LColor.R * Intensity), (int)(LColor.G * Intensity), (int)(LColor.B * Intensity));
+            return new Ray() { Origin = crossPoint, Direction = randVec };
+        }
+
+
+
+        public Color OutLight(Vector norm, Vertex crossPoint)
+        {
+            return Color.FromArgb((int)(LColor.R * Intensity), (int)(LColor.G * Intensity),
+                (int)(LColor.B * Intensity));
+        }
     }
 }
--- CompGraphics/PointLight.cs
+++ CompGraphics/PointLight.cs
@@ -2,32 +2,34 @@ using System.Drawing;
 using Core;
 using GeometricObjects.Basic;
 
-namespace CompGraphics;
-
-public class PointLight: ILighting
+namespace CompGraphics
 {
-    public Color LColor { get; set; }
-    public float Intensity { get; set; }
-    public float Fading { get; set; }
 
-    public PointLight(Vertex position, float fading = 10f)
+    public class PointLight : ILighting
     {
-        Position = position;
-        Fading = fading;
-    }
-    
-    public Vertex Position { get; set; }
-    
-    public Ray ShadowRay(Vector norm, Vertex crossPoint)
-    {
-        return new Ray() { Origin = crossPoint, Direction = Position - crossPoint };
-    }
+        public Color LColor { get; set; }
+        public float Intensity { get; set; }
+        public float Fading { get; set; }
 
-    public Color OutLight(Vector norm, Vertex crossPoint)
-    {
-        var distance = (Position - crossPoint).Abs;
-        var distanceMultiplier = Fading / (distance * distance);
+        public PointLight(Vertex position, float fading = 10f)
+        {
+            Position = position;
+            Fading = fading;
+        }
 
-        return Coloristycs.Mult(LColor, (float)(distanceMultiplier * Intensity));
+        public Vertex Position { get; set; }
+
+        public Ray ShadowRay(Vector norm, Vertex crossPoint)
+        {
+            return new Ray() { Origin = crossPoint, Direction = Position - crossPoint };
+        }
+
+        public Color OutLight(Vector norm, Vertex crossPoint)
+        {
+            var distance = (Position - crossPoint).Abs;
+            var distanceMultiplier = Fading / (distance * distance);
+
+            return Coloristycs.Mult(LColor, (float)(distanceMultiplier * Intensity));
+        }
     }
 }
